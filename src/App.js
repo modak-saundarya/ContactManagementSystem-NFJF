65a923aa4d0e9a38601aa9f851594778a625519c
Modak <saumodak@deloitte.com>
2022-07-25 12:56:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import './styles.css'
+//import { Link } from 'react-router-dom'
 function App() {
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [date,setDate] = useState("");
+  const [email,setEmail] = useState("");
+  const [address,setAddress] = useState("");
+  const [city,setCity] = useState("");
+  const [pincode,setPincode] = useState("");
+  const [mobileNo,setMobileNo] = useState("");
+
+  let submit = (e) => {
+    e.preventDefault();
+    alert(`${firstName},${date},${email},${pincode}`);
+
+    setFirstName("");
+    setLastName("");
+    setDate("");
+    setEmail("");
+    setAddress("");
+    setCity("");
+    setPincode("");
+    setMobileNo("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Heading of the project........................................ */}
+      <header>Contact Management System</header>
+      {/* Search Bar..................................................... */}
+      <div class="search">
+        <input class="search bar" type="text" placeholder="Search by Name"/>
+        <button type="submit"><img src=""/></button>
+      </div>
+
+      {/* Create Form.................................................... */}
+      <form onSubmit={submit}>
+        <section class="left-side">
+          First Name: <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}/><br/>
+          Last Name: <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/><br/>
+          DOB: <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/><br/>
+          Email ID: <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
+        </section>
+        <section class="right-side">
+          Address: <input type="text" value={address} onChange={(e) => setAddress(e.target.value)}/><br/>
+          City: <input type="text" value={city} onChange={(e) => setCity(e.target.value)}/><br/>
+          Pincode: <input type="number" value={pincode} onChange={(e) => setPincode(e.target.value)}/><br/>
+          Mobile No.: <input type="number" value={mobileNo} onChange={(e) => setMobileNo(e.target.value)}/><br/>
+        </section>
+          <input id="create_btn" type="submit" value="Create User"/>
+      </form>
     </div>
   );
 }
 
+// export function App() {
+//   return <Home/>
+// }
 export default App;

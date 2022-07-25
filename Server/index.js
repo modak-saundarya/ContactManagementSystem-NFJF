bcfa8d4f0b7725801388b87141d1a369d1123531
modak.saundarya <modak.saundarya@gmail.com>
2022-07-25 16:40:23 +0530

    changes

--- Server/index.js
+++ Server/index.js
@@ -3,4 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/create',(rq,res) => {
+    // const ADD_QUERY = 'insert into CMS.contact (cols) values (statevalues)'
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

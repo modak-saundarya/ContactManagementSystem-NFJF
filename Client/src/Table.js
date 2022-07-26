203c84de85727da4fc8095e5ad0b31f7721fc5f4
modak.saundarya <modak.saundarya@gmail.com>
2022-07-26 20:59:53 +0530

    Table

--- Client/src/Table.js
+++ Client/src/Table.js
@@ -1,3 +1,53 @@
+import { useState, useEffect } from 'react';
+
+// function TableData({name,location,id})
+
 function Table(){
-    
+    const [data,setData] = useState(null);
+    useEffect(() => {
+        fetch(
+            // 'https://api.github.com/users/modak-saundarya'
+            'https://jsonplaceholder.typicode.com/posts'
+        ).then((response) => response.json())
+        .then(setData);
+    }, []);  
+    // empty [] to make api request only once when the page renders.6.1
+    // if(data)
+    //     return(
+    //         <pre>{JSON.stringify(data,null,2)}</pre>
+    //     );
+
+    return(
+        <>
+            <h1>Table Data</h1>
+            <tbody>
+                <tr>
+                    <th>User Id</th>
+                    <th>First Name</th>
+                    <th>DOB</th>
+                    <th>Mobile No.</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                {/* <tr>
+                    <td>{data.name}</td>
+                    <td>{data.id}</td>
+                    <td>{data.created_at}</td>
+                    <td>{data.login}</td>
+
+                </tr> */}
+                {data.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.userId}</td>
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.body}</td>
+                    </tr>
+                ))}
+            </tbody>
+ 
+        </>
+    );
 }
+
+export default Table;
--- Client/src/index.js
+++ Client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Login from './Login'
+import Table from './Table'
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,8 @@ root.render(
   <Routes>
     <Route path="/" element={<App/>} />
     <Route path="/login" element={<Login/>} />
+    <Route path="/table" element={<Table/>} />
+
 
   </Routes>
     

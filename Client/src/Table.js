2f31d803edd49bc51b47b3fff748df55c6964a91
modak.saundarya <modak.saundarya@gmail.com>
2022-07-27 12:55:54 +0530

    table changes

--- Client/src/Table.js
+++ Client/src/Table.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-
+import './tableStyles.css'
 // function TableData({name,location,id})
 
 function Table(){
@@ -19,36 +19,85 @@ function Table(){
 
     return(
         <>
-            <h1>Table Data</h1>
-            <tbody>
-                <tr>
-                    <th>User Id</th>
-                    <th>First Name</th>
-                    <th>DOB</th>
-                    <th>Mobile No.</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-                <tr>
-                    <td>{data.name}</td>
-                    <td>{data.id}</td>
-                    <td>{data.created_at}</td>
-                    <td>{data.login}</td>
-
-                </tr>
+            <header>Search Results.....</header>
+            <table>
                 
-                {/* {data.map((item, i) => (
-                    <tr key={i}>
-                        <td>{item.userId}</td>
-                        <td>{item.id}</td>
-                        <td>{item.title}</td>
-                        <td>{item.body}</td>
+                <thead>
+                    <tr>
+                        <th>User Id</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>DOB</th>
+                        <th>Mobile No.</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
                     </tr>
-                ))} */}
-                
+                </thead>
+                <tbody>
+                   
+    {/* DUMMY DATA............................................................... */}
+                    <tr>
+                        <td>ID1</td>
+                        <td>D_FName1</td>
+                        <td>D_LName1</td>
+                        <td>D_DOB1</td>
+                        <td>D_Mobile1</td>
+                        <td><button class="btn"><i class="fa fa-edit"></i></button></td>
+                        <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+                    </tr>
 
-            </tbody>
- 
+                    <tr>
+                        <td>ID2</td>
+                        <td>D_FName2</td>
+                        <td>D_LName2</td>
+                        <td>D_DOB2</td>
+                        <td>D_Mobile2</td>
+                        <td><button class="btn"><i class="fa fa-edit"></i></button></td>
+                        <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+                    </tr>
+
+                    <tr>
+                        <td>ID3</td>
+                        <td>D_FName3</td>
+                        <td>D_LName3</td>
+                        <td>D_DOB3</td>
+                        <td>D_Mobile3</td>
+                        <td><button class="btn"><i class="fa fa-edit"></i></button></td>
+                        <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+                    </tr>
+
+                    <tr>
+                        <td>ID4</td>
+                        <td>D_FName4</td>
+                        <td>D_LName4</td>
+                        <td>D_DOB4</td>
+                        <td>D_Mobile4</td>
+                        <td><button class="btn"><i class="fa fa-edit"></i></button></td>
+                        <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+                    </tr>
+
+
+                    {/* <tr>
+                        <td>{data.name}</td>
+                        <td>{data.id}</td>
+                        <td>{data.created_at}</td>
+                        <td>{data.login}</td>
+
+                    </tr> */}
+                    
+                    {/* {data.map((item, i) => (
+                        <tr key={i}>
+                            <td>{item.userId}</td>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.body}</td>
+                        </tr>
+                    ))} */}
+                    
+
+                </tbody>
+    
+            </table>
         </>
     );
 }

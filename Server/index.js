0b55fc680562ceb2c00d287546532fd8149d02bd
anshulkhilrani <khilranianshul@gmail.com>
2022-07-27 16:28:51 +0530

    enter data

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,45 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+var config = require("./dbconfig");
+const sql = require('mssql');
 const app = express()
 const port = 4000
 
+app.use(cors())
+app.use(bodyParser.json())
 
+//Establish a Connection................
+sql.connect(config, err => { 
+    if(err){
+        throw err ;
+    }
+    console.log("Connection Successful !");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+        
+});
+
+//Basic Structure for APIs
+app.get('/',(req,res) => {
+    new sql.Request().query('select * from dbo.contact', (err, result) => {
+        //handle err
+        if(err) console.log(err)
+
+        res.send(result);
+    })
+})
+
+app.post('/create',(req,res) => {
+    const INSERT_QUERY = 'Insert into dbo.contact (FirstName,Email,ContactNo) values(${req.body.firstName},${req.body.email},${req.body.mobileNo});'
+    new sql.query(INSERT_QUERY, (err, result) => {
+        //handle err
+        if(err) console.log(err)
+
+        res.send(result);
+    })
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})

a96178648c74b96d9ec81eca4fd15c6fd8518c4d
anshulkhilrani <khilranianshul@gmail.com>
2022-07-27 09:57:44 +0530

    basic changes

---
+++ Server/table.js
@@ -0,0 +1,15 @@
+class Table{
+    constructor(customerID, firstname, lastname, dob, address, city, pin, contactno, email){
+        this.customerID = customerID;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dob = dob;
+        this.address = address;
+        this.city = city;
+        this.pin = pin;
+        this.contactno = contactno;
+        this.email = email;
+    }
+}
+
+module.exports = Table;

ea2c95d57bde30e50ee4b3c9c11b33b16a6cd6de
modak.saundarya <modak.saundarya@gmail.com>
2022-07-28 20:05:58 +0530

    changes again

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
-import './styles.css'
+import './styles.css';
+import { Link } from 'react-router-dom';
 import Table from './Table';
 import AddContact from './AddContact';
 
@@ -13,7 +14,16 @@ function App() {
     <div className="App">
       {/* Heading of the project........................................ */}
       <header>Contact Management System</header>
-      
+      <div>
+        <ul>
+          <li><Link to="/">Add Contact</Link> </li>
+          <li><Link to="/edit">Edit Contact</Link> </li>
+          <li><Link to="/table">Search Contact </Link> </li>
+
+
+        </ul>
+      </div>
+
       
       <AddContact/>
 

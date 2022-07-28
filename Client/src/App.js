b1edb0f89a00936b9bdb3fd4d28074e9ed4730dc
modak.saundarya <modak.saundarya@gmail.com>
2022-07-28 19:38:50 +0530

    client changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,35 +8,13 @@ import AddContact from './AddContact';
 
 
 function App() {
-  
-  // Form Values............................................................
-  // Search Values.......................................
-  const [searchName,setSearchName] = useState("");
-
-  let searchSubmit = async (e) =>{
-    e.preventDefault();
-    console.warn({searchName});
-    let result = await fetch("http://localhost:4000/search/${searchName}");
-    result = await result.json();
-    // if(result){
-      // Table(result);
-    // }
-
-    setSearchName("");
-
-
-  }
-
 
   return (
     <div className="App">
       {/* Heading of the project........................................ */}
       <header>Contact Management System</header>
-      {/* Search Bar..................................................... */}
-      <div class="search">
-        <input class="search bar" type="text"  value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Search by FirstName"/>
-        <button onClick={searchSubmit} type="submit"><i class="fa fa-search"></i></button>
-      </div>
+      
+      
       <AddContact/>
 
       {/* Create Form.................................................... */}
